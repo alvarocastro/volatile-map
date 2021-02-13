a960ad2acf16feb24f8b0f8bdb866f6d3ba6a209
Alvaro Castro <moltar89@gmail.com>
2021-02-12 23:47:19 -0300

    feat: custom ttl on an item per item basis

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const DEFAULT_TTL = 600000; // 10 minutes
 
 class VolatileMap extends Map {
-	constructor (ttl) {
+	constructor(ttl) {
 		super();
 		this.ttl = ttl ?? DEFAULT_TTL;
 		this.schedules = new Map();
 	}
 
-	delete (key) {
+	delete(key) {
 		super.delete(key);
 		clearTimeout(this.schedules.get(key));
 		this.schedules.delete(key);
 	}
 
-	set (key, value, ttl = this.ttl) {
+	set(key, value, ttl = this.ttl) {
 		super.set(key, value);
 		if (this.schedules.has(key)) {
 			clearTimeout(this.schedules.get(key));
@@ -25,7 +25,7 @@ class VolatileMap extends Map {
 		this.schedules.set(key, schedule);
 	}
 
-	get (key) {
+	get(key) {
 		return super.get(key);
 	}
 }
